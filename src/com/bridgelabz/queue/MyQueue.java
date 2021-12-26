8cb315e1b54d221dc32547925216eb26e1c33666
lokesh <kottelokesh789@gmail.com>
2021-12-26 16:07:01 +0530

    ADDED: CreatedAQueue

---
+++ src/com/bridgelabz/queue/MyQueue.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.queue;
+
+import com.bridgelabz.linkedlist.INode;
+import com.bridgelabz.linkedlist.MyLinkedList;
+
+public class MyQueue {
+private final MyLinkedList myLinkedList;
+	
+	public MyQueue() {
+		this.myLinkedList = new MyLinkedList();
+	}
+	public void enqueue(INode newNode) {
+		myLinkedList.append(newNode);
+	}
+	public void printQueue() {
+		myLinkedList.printMyNodes();
+	}
+
+}
