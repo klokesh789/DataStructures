316dcd108e1c2755032d898b2c8a8a3941054bf6
lokesh <kottelokesh789@gmail.com>
2021-12-26 21:47:47 +0530

    ADDED: CreatedABinarySearchTree

---
+++ src/com/bridgelabz/binarysearchtree/BinaryTreeMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.binarysearchtree;
+
+public class BinaryTreeMain {
+	public static void main(String[] args) {
+
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+
+        System.out.println("Binary Tree is :");
+        myBinaryTree.print();
+    }
+
+}
